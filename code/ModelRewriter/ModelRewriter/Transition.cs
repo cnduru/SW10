9bdfe527b45821430bc9bdf3cc3ea17c4a3ff069
cnduru11@student.aau.dk <cnduru11@student.aau.dk>
2016-02-19 13:22:12 +0100

    fixed bug

--- code/ModelRewriter/ModelRewriter/Transition.cs
+++ code/ModelRewriter/ModelRewriter/Transition.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace ModelRewriter
 {
@@ -10,5 +11,20 @@ namespace ModelRewriter
     {
         public string source { get; set; }
         public string target { get; set; }
+
+        public XElement getXML()
+        {
+            XElement transitionElement = new XElement("transition");
+            XElement srcElement = new XElement("source");
+            XElement targetElement = new XElement("target");
+
+            srcElement.SetAttributeValue("ref", source);
+            targetElement.SetAttributeValue("ref", target);
+
+            transitionElement.Add(srcElement);
+            transitionElement.Add(targetElement);
+
+            return transitionElement;
+        }
     }
 }
