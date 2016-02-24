b29978992c919737860216ad9b36b53cb7e27403
cnduru11@student.aau.dk <cnduru11@student.aau.dk>
2016-02-24 10:25:10 +0100

    dunno why these are here

---
+++ code/ModelRewriter/ModelRewriter/Label.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Xml.Linq;
+
+
+namespace ModelRewriter
+{
+    public class Label
+    {
+        public string content;
+        public string kind;
+        public int x;
+        public int y;
+
+        public void SetCords(Location loc, int offset)
+        {
+            x = Constants.LabelOffsetX;
+            y = Convert.ToInt32(loc.y) + offset;
+        }
+
+        public XElement GetXML()
+        {
+            var tmp = new XElement("label");
+            tmp.SetAttributeValue("kind", kind);
+            tmp.SetAttributeValue("x", x);
+            tmp.SetAttributeValue("y", y);
+            tmp.SetValue(content);
+            return tmp;
+        }
+    }
+}
+
