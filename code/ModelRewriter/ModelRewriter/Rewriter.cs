3b87db5447a9f931b4509bd2117d6e6d376fe3f6
cnduru11@student.aau.dk <cnduru11@student.aau.dk>
2016-02-26 12:43:48 +0100

    removed comments.. again

--- code/ModelRewriter/ModelRewriter/Rewriter.cs
+++ code/ModelRewriter/ModelRewriter/Rewriter.cs
@@ -12,33 +12,8 @@ namespace ModelRewriter
         private List<Template> _templates;
 
         public Rewriter(string path)
-        {/*
-            // read templates from XML file
-            XDocument doc = XDocument.Load(@"C:\Users\Avalon\SW10\code\models\sample_timed.xml");
-            XMLHandler handler = new XMLHandler(doc);
-
-            // read UPPAAL model
-            string text = System.IO.File.ReadAllText(path);
-            
-            // get index of declarations and insert new fault template
-            int declIndex = text.IndexOf("</declaration>");
-            string res = text.Insert(declIndex + 14, "\n" + XMLProvider.getFaultTemplate());
-
-            // insert global variable
-            res = res.Insert(declIndex, "\n" + "int faultAt = 0;\n");
-
-            // add fault process to system
-            res = res.Insert(res.IndexOf("template instantiations here.") + 29, "\nFault = FaultInj();");
-            res = res.Insert(res.IndexOf("composed into a system.") + 31, " Fault,");
-
-            // write file to disk
-            using (System.IO.StreamWriter file = new System.IO.StreamWriter("sampleGenerated.xml"))
-            {
-                file.Write(res);
-            }
-            */
-
-            UppaalModel uml = new UppaalModel("sample.xml");
+        {
+            UppaalModel uml = new UppaalModel("sample_timed.xml");
             uml.Save("testxml.xml");
         }
     }
