0e4c33f2773d18bb21320a6ea1fb90743a664130
cnduru11@student.aau.dk <cnduru11@student.aau.dk>
2016-03-01 10:08:34 +0100

    removed outcommented code

--- code/ModelRewriter/ModelRewriter/UppaalModel.cs
+++ code/ModelRewriter/ModelRewriter/UppaalModel.cs
@@ -94,30 +94,6 @@ system s, s1;";
         {
             // quick fix, probably should have an addDeclaration method that adds to a global stringbuilder
             return declarations += "\n" + "int faultAt = 0;\n";
-            /*
-            // read templates from XML file
-            XDocument doc = XDocument.Load(@"C:\Users\Avalon\SW10\code\models\sample_timed.xml");
-            XMLHandler handler = new XMLHandler(doc);
-
-            // read UPPAAL model
-            string text = System.IO.File.ReadAllText(path);
-
-            // get index of declarations and insert new fault template
-            int declIndex = text.IndexOf("</declaration>");
-            string res = text.Insert(declIndex + 14, "\n" + XMLProvider.getFaultTemplate());
-
-            // insert global variable
-            res = res.Insert(declIndex, "\n" + "int faultAt = 0;\n");
-
-            // add fault process to system
-            res = res.Insert(res.IndexOf("template instantiations here.") + 29, "\nFault = FaultInj();");
-            res = res.Insert(res.IndexOf("composed into a system.") + 31, " Fault,");
-
-            // write file to disk
-            using (System.IO.StreamWriter file = new System.IO.StreamWriter("sampleGenerated.xml"))
-            {
-                file.Write(res);
-            }*/
         }
 
         private string getSystem(string stem)
