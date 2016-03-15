76100bba08fa003f836cb36959ec6cb8d27819ff
Kristian <kthoms10@student.aau.dk>
2016-03-15 13:32:37 +0100

    should ingnore html soon, added sources for invoke and opstack pointer

--- code/ocaml/java/Sample.java
+++ code/ocaml/java/Sample.java
@@ -1,8 +1,9 @@
 public class Sample{
     public static void main(String[] args) {
         A test = new B();
-        for(short i=0; i<5; i++){
+        for(short i=0; i<3; i++){
               test.add(i);
         }
+        int i = test.get_value();
     }
 }
