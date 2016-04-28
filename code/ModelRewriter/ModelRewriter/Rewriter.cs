64d05569eade2d08dbad066bb3b7bd39506878d9
cnduru11@student.aau.dk <cnduru11@student.aau.dk>
2016-04-28 12:48:52 +0200

    parameterized local and opstack faults

--- code/ModelRewriter/ModelRewriter/Rewriter.cs
+++ code/ModelRewriter/ModelRewriter/Rewriter.cs
@@ -12,7 +12,7 @@ namespace ModelRewriter
         public Rewriter(string path)
         {
             UppaalModel uml;
-            // countermeasure parameter has to fit UPPAAL rewrite mode - e.g. "pc" for PCFault and "data" for DataFault
+            // countermeasure parameter has to fit UPPAAL rewrite mode - e.g. "pc" for PCFault and "heap" for heap fault
             // should we handle Uppaal model generation without faults?
             //uml = new UppaalModel(path, "pc");
             //uml.rewritePCFault("pcRewrite.xml");
@@ -20,13 +20,15 @@ namespace ModelRewriter
             //uml = new UppaalModel(path, "heap");
             //uml.rewriteHeapFault("heapRewrite.xml");
 
-            uml = new UppaalModel(path, "locals");
-            uml.rewriteLocalFault("localRewrite.xml");
+            //uml = new UppaalModel(path, "locals");
+            //uml.rewriteLocalFault("localRewrite.xml");
 
             //uml = new UppaalModel(path, "instruction");
             //uml.addErrorLocation();
             //uml.rewriteInstructionFault("instructionRewrite.xml");
 
+            uml = new UppaalModel(path, "opstack");
+            uml.rewriteOpstackFault("opstackRewrite.xml");
         }
     }
 }
