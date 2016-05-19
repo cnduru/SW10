5a3dc911859893a755b5bc4f7a02f51ad10fc812
cnduru11@student.aau.dk <cnduru11@student.aau.dk>
2016-05-19 10:44:37 +0200

    ExampleCFI.jar

---
+++ code/ocaml/java/ExampleCFI.java
@@ -0,0 +1,65 @@
+public class ExampleCFI
+{
+	private int flag = 0;
+	
+    public static void main(String[] args) {
+        try
+        {
+		  ExampleCFI hw = new ExampleCFI();
+        }catch (Exception ex)
+        {
+
+        }
+    }
+
+    public ExampleCFI() throws Exception
+    {
+		// CFI START
+        processVerifyPIN();
+		
+		if(flag != 3)
+		{
+			throw new Exception();
+		}
+    }
+
+    private void processVerifyPIN() throws Exception
+    {
+		flag++;
+		
+        int pinLength = 4;
+        int faultCode = 255;
+        int triesRemaining;
+
+        short count = setIncomingAndReceive();    // get expected data
+		
+        if(count < pinLength) throw new Exception();
+
+        if(isInvalid() != false)
+        {
+            triesRemaining = getTriesRemaining();
+            throw new Exception();
+        }
+    }
+
+
+    private boolean isInvalid()
+    {
+		flag++;
+        return true;
+    }
+
+    private short setIncomingAndReceive()
+    {
+		flag++;
+        return 5;
+    }
+
+    private int getTriesRemaining()
+    {
+        return 2;
+    }
+
+
+}
+
